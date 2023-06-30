76bfab3c1ac201f7413c8cbbba688efbe0ebf1be
George <gnjuki19@gmail.com>
2023-06-30 12:05:31 +0300

    Started the hackathon project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootswatch/dist/cyborg/bootstrap.min.css"; // Added this :boom:
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Team from "./Pages/Team";
+import { useEffect } from "react";
+
+const CLIENT_ID = "bFrxaipzwQkKZgolvUuntBUHvRhw5G4hkju5OVHr";
+const SECRET_KEY =
+  "WQqHwb09lDd5emNMTAKWbvDpQlJp6QmYMU2CWP8FwcopIorjYD8rKeNL1hHEUU37tJX6jOzgkSA7GaBLNUg7jZKuyq5r9JDlGUE6R7h4HI7Lb4QJgZj9sXS7VTeexTmk";
 
 function App() {
+  const URL = "https://accounts.multitenant.slade360.co.ke/oauth2/token/";
+
+  // const data = {
+  //   grant_type: "password",
+  //   client_id: "bFrxaipzwQkKZgolvUuntBUHvRhw5G4hkju5OVHr",
+  //   client_secret:
+  //     "WQqHwb09lDd5emNMTAKWbvDpQlJp6QmYMU2CWP8FwcopIorjYD8rKeNL1hHEUU37tJX6jOzgkSA7GaBLNUg7jZKuyq5r9JDlGUE6R7h4HI7Lb4QJgZj9sXS7VTeexTmk",
+  //   username: "gnjuki19@gmail.com",
+  //   password: "Moringa@2022",
+  // };
+  const clientData = `grant_type=password&client_id=${CLIENT_ID}&client_secret=${SECRET_KEY}&username=gnjuki19@gmail.com&password=Moringa@2022`;
+
+  const getAuthToken = () => {
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: clientData,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAuthToken();
+  }, []);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+      <h1>Hello React</h1>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+          <Link to='/' className="navbar-brand">HealthCloud</Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor02"
+            aria-controls="navbarColor02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <Link to="/" className="nav-link active">Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link to='/about' className="nav-link">About</Link>
+              </li>
+              <li class="nav-item">
+                <Link to='/team' className="nav-link">Team</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
