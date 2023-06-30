f70ef9593d9fad79ed316b36c7e2ab4ccb463c76
George <gnjuki19@gmail.com>
2023-06-30 12:47:56 +0300

    Added search member function

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,48 +1,53 @@
-import React from 'react'
+import React from "react";
 
-const Home = ({memberNumber, payerSladeCode, setMemberNumber, setPayerSladeCode}) => {
+const Home = ({
+  memberNumber,
+  payerSladeCode,
+  setMemberNumber,
+  setPayerSladeCode,
+  searchUser,
+}) => {
   return (
     <div>
-       <form onSubmit={searchUser}>
-          <div className="mb-3">
-            <label htmlFor="memberNumber" className="form-label">
-              Member Number:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="memberNumber"
-              placeholder="Enter member number here..."
-              value={memberNumber}
-              onChange={(e) => setMemberNumber(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="payerSladeCode" className="form-label">
-              Slade Code:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="payerSladeCode"
-              placeholder="Enter slade code here..."
-              value={payerSladeCode}
-              onChange={(e) => setPayerSladeCode(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">
-            Search
-          </button>
-         
-        </form>
+      <form onSubmit={searchUser}>
+        <div className="mb-3">
+          <label htmlFor="memberNumber" className="form-label">
+            Member Number:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="memberNumber"
+            placeholder="Enter member number here..."
+            value={memberNumber}
+            onChange={(e) => setMemberNumber(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="payerSladeCode" className="form-label">
+            Slade Code:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="payerSladeCode"
+            placeholder="Enter slade code here..."
+            value={payerSladeCode}
+            onChange={(e) => setPayerSladeCode(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-outline-primary">
+          Search
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-
-
- {/* <Link to="/membershipCard" className="btn btn-outline-info m-2">
+{
+  /* <Link to="/membershipCard" className="btn btn-outline-info m-2">
             View Deatils
-          </Link> */}
+          </Link> */
+}
